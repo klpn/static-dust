d451e5aa8c3fd82eefe4100dd445b61b6ee64b61
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2020-03-08 18:38:23 +0100

    Updated post; changed math font to Asana

--- postdata/2020-03-07-mottag/sinfplot.jl
+++ postdata/2020-03-07-mottag/sinfplot.jl
@@ -4,13 +4,13 @@ ss = map(x -> x/100, 1:100)
 sinf(r0) = map(s -> r0*(s-1) - log(s), ss) 
 p = @pgf Axis({"xticklabel style"={"/pgf/number format/use comma"},
    legend_pos="north east",
-   title=*(L"S_\infty", " för varierande ", L"R_0"), xlabel=L"S",
-   ylabel=L"R_0(S-1)-\log(S)",
+   title=*(L"S_\infty", " för varierande ", L"\mathcal{R}_0"), xlabel=L"S",
+   ylabel=L"\mathcal{R}_0(S-1)-\log(S)",
    xmajorgrids, ymajorgrids})
 rs = [1.3, 1.8, 2, 2.5, 3]
 plotcolors = distinguishable_colors(length(rs)+1, [RGB(1,1,1)])[2:end]
 for (i, r) in enumerate(rs)
-   rlab = latexstring(replace("R_0=$r", "."=>"{,}"))
+   rlab = latexstring(replace("\\mathcal{R}_0=$r", "."=>"{,}"))
    @pgf push!(p, PlotInc({no_markers, color = plotcolors[i]},
       Table([ss, sinf(r)])),
       LegendEntry(rlab))
