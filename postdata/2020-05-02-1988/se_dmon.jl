f7d8e1d286b59cb15bf15094e7b627b7b09cd8ac
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2020-05-02 11:00:04 +0200

    New post

---
+++ postdata/2020-05-02-1988/se_dmon.jl
@@ -0,0 +1,25 @@
+using Colors, CSV, DataFrames, PGFPlotsX
+se_dmon = CSV.File("data/se_dmon1851_2019.tsv", delim="\t") |> DataFrame
+
+function plotmonths(sex, months, syr, eyr)
+    sax = @pgf Axis({"yticklabel style"={"/pgf/number format/use comma"},
+              "xticklabel style"={"/pgf/number format/set thousands separator={}"},
+              "yticklabel style"={"/pgf/number format/set thousands separator={}"},
+              "legend style"={"font=\\tiny"},
+              legend_pos="outer north east", ylabel="Döda",
+              title = "Antal dödsfall per månad $(sex) Sverige",
+              xmajorgrids, ymajorgrids})
+    plotcolors = distinguishable_colors(length(months)+1, [RGB(1,1,1)])[2:end]
+    for (i, month) in enumerate(months)
+        plotmonth(sex, month, syr, eyr, sax, plotcolors[i])
+    end
+    sax                       
+end
+
+function plotmonth(sex, month, syr, eyr, p, plotcolor)
+    se_dmon_sub = se_dmon[((se_dmon[:kön].==sex) .& (se_dmon[:månad].==month)
+        .& (se_dmon[:år].>=syr) .& (se_dmon[:år].<=eyr)), :]
+    @pgf push!(p, PlotInc({no_markers, color = plotcolor},
+        Table([se_dmon_sub[:år], se_dmon_sub[:Döda]])),
+        LegendEntry(month))
+end
---
+++ postdata/2020-05-02-1988/se_dmon_20_19.jl
@@ -0,0 +1,10 @@
+#!/usr/bin/env julia
+include("se_dmon.jl")
+push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{fontspec}")
+push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\setmainfont{Asana-Math}")
+flumonths = ["oktober", "november", "december", "januari",
+   "februari", "mars", "april"]
+pgfsave("../../images/se_dmon_20_19_kv.svg",
+   plotmonths("kvinnor", flumonths, 1920, 2019))
+pgfsave("../../images/se_dmon_20_19_m.svg",
+   plotmonths("män", flumonths, 1920, 2019))
