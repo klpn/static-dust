0311ee39d3d94572c948cdd9becc3373935097ae
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2020-07-28 14:43:54 +0200

    Moved CSV inte data directory

--- postdata/2020-07-28-likt/infl_se_reg_98_18.jl
+++ postdata/2020-07-28-likt/infl_se_reg_98_18.jl
@@ -2,7 +2,7 @@
 include("sossltv.jl")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{fontspec}")
 push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\setmainfont{Asana-Math}")
-s = sosframe("Statistikdatabasen_2020-07-28 11 46 15.csv")
+s = sosframe("data/Statistikdatabasen_2020-07-28 11 46 15.csv")
 pgfsave("../../images/infl_se_reg_kv65_98_18.svg",
     sexplot(s, "kvinnor"))
 pgfsave("../../images/infl_se_reg_m65_98_18.svg",
