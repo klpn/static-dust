d197f955078076052328f079f21353b3ef88c40d
Karl Pettersson <karl.pettersson@klpn.se>
2024-03-31 22:34:01 +0200

    Corrected X label in figures

--- postdata/2024-03-31-post/covnorm.r
+++ postdata/2024-03-31-post/covnorm.r
@@ -6,14 +6,14 @@ cov085_sum_fy <- inflframe("data/cov_se_par_085.tsv", "U0[7-9]") |> inflfdgrp(TR
 infl085_sum_fy_pop <- inflfnorm(infl085_sum_fy, pop085, c("Sex","sage","sdate"))
 cov085_sum_fy_pop <- inflfnorm(cov085_sum_fy, pop085, c("Sex","sage","sdate"))
 cov085_sum_fy_pop |>
-    inflplot("2018-02-01", "covid-19", "säsong  (juli år X−1 till juni år X, 2023 preliminärt till december)",
+    inflplot("2018-02-01", "covid-19", "säsong  (juli år X till juni år X+1, 2023 preliminärt till december)",
              "incidens")
 ggsave("../../images/covpop.svg")
 infl085_sum_fy_pop |>
-    inflplot("1996-02-01", "influensa", "säsong  (juli år X−1 till juni år X, 2023 preliminärt till december)",
+    inflplot("1996-02-01", "influensa", "säsong  (juli år X till juni år X+1, 2023 preliminärt till december)",
              "incidens")
 ggsave("../../images/inflpop.svg")
 inflfnorm(cov085_sum_fy_pop, inflfmgrp(infl085_sum_fy_pop, max), c("Sex", "sage")) |>
-    inflplot("2018-01-01", "covid-19", "säsong  (juli år X−1 till juni år X, 2023 preliminärt till december)",
+    inflplot("2018-01-01", "covid-19", "säsong  (juli år X till juni år X+1, 2023 preliminärt till december)",
              "incidens/max(incidens(influensa, 1998–2023)")
 ggsave("../../images/covnorm.svg")
